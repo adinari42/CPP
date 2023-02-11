6daa65d1f34b964d00eca9f634c84f6e7e5dee4b
adinari <adinari@student.42heilbronn.de>
2023-02-11 18:58:41 +0100

    syntax adjust , explanation comments addition

--- Module_02/ex01/Fixed.cpp
+++ Module_02/ex01/Fixed.cpp
@@ -6,28 +6,38 @@ Fixed::Fixed(void) : fixed_point_val(0)
 	std::cout << "Default constructor called" << std::endl;
 }
 
+/*constructors*/
+
+//in both int and float cases we are bitshifting to the left to create margin
+//and eliminate the fractional part until it's brought back later
 Fixed::Fixed(const int intNum)
 {
+	//bitshifting to the left
 	fixed_point_val = intNum << fract_bit;//bitshifting to the left by fract_bit bits
 }
 
 Fixed::Fixed(const float floatNum)
 {
+	/*float method of bitshifting to the left , plus roundf to get the nearest first digit which is 2
+	or else it will be incorrect (42.418)*/
 	fixed_point_val = roundf(floatNum * (1 << fract_bit));
 }
+/**************/
 
 // that converts the fixed-point value to a floating-point value.
 float Fixed::toFloat( void ) const
 {
 	//division by 2^fract_bit bitshifts the number to the right, creating fractional part
 	float floatRes = static_cast<float>(fixed_point_val) / static_cast<float>(1 << fract_bit);
+	//we cant bitshift / divide a float by 2^8 so we have to bitshift the int 1 by 2^8
+	//then divide the float by it
 	return (floatRes);
 }
 
 // that converts the fixed-point value to an integer value
 int Fixed::toInt( void ) const
 {
-	//multiplication by 2^fract_bit binary shifts the number to the right, creating integer part
+	//division by 2^fract_bit binary shifts the number to the right, creating integer part
 	int intRes = fixed_point_val >> fract_bit;
 	return (intRes);
 }
@@ -53,6 +63,10 @@ Fixed &Fixed::operator=(const Fixed &a)
 // of the fixed-point number into the output stream object passed as parameter
 std::ostream &operator<<(std::ostream& os, const Fixed &a)
 {
+	/*
+		uses toFloat() to devide the number by 2^8 because the current value is
+		multiplied by 2^8 and is bigger than the original.
+	*/
 	// os << "insertion assignment operator called" << std::endl;
 	os << a.toFloat();
 	return(os);
