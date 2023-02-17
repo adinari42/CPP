1f3d116b21545c6b83151605b28e21bb1e089353
adinari <adinari@student.42heilbronn.de>
2023-02-17 20:36:25 +0100

    fixed stringREF adress print, added comments

--- Module_01/ex02/main.cpp
+++ Module_01/ex02/main.cpp
@@ -8,7 +8,7 @@ int	main()
 
 	std::cout << "\033[31mThe memory adress of the string variable >>\033[0m " << &str << std::endl;
 	std::cout << "\033[31mThe memory adress held by stringPTR      >>\033[0m " << stringPTR << std::endl;
-	std::cout << "\033[31mThe memory adress held by stringREF      >>\033[0m " << stringPTR << std::endl;
+	std::cout << "\033[31mThe memory adress held by stringREF      >>\033[0m " << &stringREF << std::endl;
 
 	std::cout << "\033[31mThe value of the string variable  $:\033[0m " << str << std::endl;
 	std::cout << "\033[31mThe value pointed to by stringPTR $:\033[0m " << *stringPTR << std::endl;
--- Module_01/ex04/main.cpp
+++ Module_01/ex04/main.cpp
@@ -9,14 +9,15 @@ std::string replace_occ(std::string s, std::string x, std::string y)
 	size_t pos = 0;
 	while (true)
 	{
-		pos = s.find(x, pos);
+		pos = s.find(x, pos);//finds the position where x occures in s string
+		//if it doesn't exist , find returns npos
 		if (pos == std::string::npos) {
 			break;
 		}
 
-		s.erase(pos, x.length());
-		s.insert(pos, y);
-		pos += y.length();
+		s.erase(pos, x.length());//erases x.length characters from that position
+		s.insert(pos, y);//inserts y string in that position
+		pos += y.length();//updated pos to start from the end of the inserted string
 	}
     return s;
 }
@@ -30,11 +31,11 @@ int main(int argc, char **argv)
 		return (1); 
 	}
     std::string		line;
-    std::ifstream	ini_file (argv[1]);
+    std::ifstream	ini_file (argv[1]);//opens infile stream with the name of file passes as param
 	std::string		out_fd = argv[1];
     if (ini_file)
 	{
-  
+  		//this declaration opens an outfile with the parameter string as name
     	std::ofstream	out_file (out_fd.append(".replace"));
 		if (!out_file.write("write permission test", 0))
 		{
